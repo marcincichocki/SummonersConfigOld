9faf4ead6842989acb9c13b49f6bae53bb711f58
marcincichocki <marcincichocki1993@gmail.com>
2015-10-30 19:40:49 +0100

    Search box component

---
+++ public/assets/app/search.component.ts
@@ -0,0 +1,13 @@
+import {Component, FORM_DIRECTIVES} from 'angular2/angular2';
+
+
+@Component({
+  directives: [FORM_DIRECTIVES],
+  selector: 'search-component',
+  template: `
+    <input type="text" [(ng-model)]="query">
+  `
+})
+export class SearchComponent {
+  public query: string = '';
+}
