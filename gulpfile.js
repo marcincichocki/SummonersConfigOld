2e5c0af6edf7764b6d5fd97def58650d81e62ee8
marcincichocki <marcincichocki1993@gmail.com>
2015-11-01 23:48:25 +0100

    Installed gulp and some plugins for styles(sass)

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
+
+
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+  gulp.watch('./public/assets/sass/**/*.scss', ['styles']);
+});
+
+
+gulp.task('styles', function() {
+  return gulp.src('./public/assets/sass/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(rename('style.min.css'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+    .pipe(minifyCss())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/assets/css/'));
+});
