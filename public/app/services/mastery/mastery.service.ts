0f756ea6504aeda8fca28eb99f61144dd87f99e9
marcincichocki <marcincichocki1993@gmail.com>
2015-12-02 19:06:50 +0100

    Added comments to mastery service.

--- public/app/services/mastery/mastery.service.ts
+++ public/app/services/mastery/mastery.service.ts
@@ -8,8 +8,14 @@ import {TooltipService} from '../tooltip/tooltip.service';
 
 @Injectable()
 export class MasteryService extends Pages<Page> {
+
+  // Object which contains grid of masteries.
   public grid: any;
+
+  // Object which contains every mastery.
   public masteries: any;
+
+  // List of categories.
   public categories: string[] = [
     'Ferocity',
     'Resolve',
@@ -17,14 +23,25 @@ export class MasteryService extends Pages<Page> {
   ];
 
 
+  /**
+   * Constructor function.
+   */
   constructor(
     public http: Http,
     public tooltipService: TooltipService
   ) {
+
+    // Create data structure.
     super(Page, 'Mastery Page');
+
+    // By defaut, create one page.
     this.addPage();
   }
 
+
+  /**
+   * Load masteries and save into variables.
+   */
   getMasteries() {
     this.http.get('./app/data/en/masteries.json')
       .map(res => res.json())
@@ -37,19 +54,11 @@ export class MasteryService extends Pages<Page> {
       );
   }
 
-  isEven(n: number): boolean {
-    return n % 2 === 0;
-  }
-
-  addMastery(id: number): void {
-    const {category, row} = this.getInfo(id);
-
-    if (this.masteryCheckUp(id, category, row)) {
-      this.current.addMastery(id, category);
-      this.updateTooltip(id);
-    }
-  }
-
+  /**
+   * Get category and row based for given mastery id.
+   * @param {number} id - unique mastery id.
+   * @return {Object} - Object which contains category and row properties.
+   */
   getInfo(id: number): {category: number, row: number} {
     return {
       category: parseInt(id.toString().slice(1, 2), 10),
@@ -57,32 +66,115 @@ export class MasteryService extends Pages<Page> {
     }
   }
 
-  removeMastery(id: number): void {
+
+  /**
+   * Add new point for given mastery id.
+   * @param {number} id - Unique mastery id.
+   */
+  addMastery(id: number): void {
+
+    // Get category and row of mastery.
     const {category, row} = this.getInfo(id);
 
-    if (this.masteryCheckDown(id, category, row)) {
-      this.pages[this.active].removeMastery(id, category);
+    // Continue only if it is possible to add point.
+    if (this.masteryCheckUp(id, category, row)) {
+
+      // Add/update mastery.
+      this.current.addMastery(id, category);
+
+      // Refresh tooltip.
       this.updateTooltip(id);
     }
   }
 
+
+  /**
+   * Remove point for given mastery id.
+   * @param {number} id - Unique mastery id.
+   */
+  removeMastery(id: number): void {
+
+    // Get category and row of mastery.
+    const {category, row} = this.getInfo(id);
+
+    // Continue only if it is possible to remove point.
+    if (this.masteryCheckDown(id, category, row)) {
+
+      // Remove/update mastery.
+      this.current.removeMastery(id, category);
+
+      // Refresh tooltip.
+      this.updateTooltip(id);
+    }
+  }
+
+
+  /**
+   * Check if it is possible to add new point for given mastery id.
+   * @param  {number} id - Unique mastery id.
+   * @param  {number} [category] - Category id for given mastery id.
+   * @param  {number} [row] - Row id for given mastery id.
+   * @return {boolean} Return true if mastery is possible to add,
+   * false otherwise.
+   */
   masteryCheckUp(id: number, category?: number, row?: number): boolean {
+
+    /**
+     * Get category and row of given mastery id, if category either row
+     * are not numbers(this also works if they are not defined at all).
+     */
     if (isNaN(category) || isNaN(row)) {
+
+      // var because block scope.
       var {category, row} = this.getInfo(id);
     }
 
+
+    /**
+     * Here is how mastery enabling works.
+     *
+     * First, check if max available points for current page are more then 0.
+     * Max points are capped at 30(or summoner level) and decrement by one
+     * for each mastery point added.
+     *
+     * Second, check if sum of points in given row and category are less then
+     * max(5 for odd, 1 for even).
+     */
     if (this.current.max > 0 && !this.isMaxed(category, row)) {
-      if (row !== 1 && this.isMaxed(category, row - 1)) {
-        return true;
-      } else if (row === 1) {
+
+      /**
+       * Only then check if the row is NOT 1st and previous row is maxed
+       * or the row is 1st.
+       */
+      if ((row !== 1 && this.isMaxed(category, row - 1)) || row === 1) {
+
+        // If both cases are fulfilled, return true.
         return true;
       }
     }
+
+    // Otherwise return false.
     return false;
   }
 
 
+  /**
+   * Check if it is possible to remove point for given mastery id.
+   * @param  {number} id - Unique mastery id.
+   * @param  {number} category - Category id for given mastery id.
+   * @param  {number} row - Row id for given mastery id.
+   * @return {boolean} Return true if mastery is possible to remove,
+   * false otherwise.
+   */
   masteryCheckDown(id: number, category: number, row: number): boolean {
+
+    /**
+     * Return true only if given statments are true:
+     * 1. Max available points for current page are not more then max
+     * overall(or summoner level).
+     * 2. Rank of mastery for given mastery id is more then 0.
+     * 3. Sum of next row is equal 0(there is no points added).
+     */
     if (
       this.current.max < 30
       && this.current.rank[id] > 0
@@ -90,26 +182,61 @@ export class MasteryService extends Pages<Page> {
     ) {
       return true;
     }
+
+    // Otherwise return false.
     return false;
   }
 
+
+  /**
+   * Check if given row in given category is maxed.
+   * @param  {number} category - Category id for given mastery id.
+   * @param  {number} row - Row id for given mastery id.
+   * @return {boolean} Return true if row is maxed, false otherwise.
+   */
   isMaxed(category: number, row: number): boolean {
+
+    // Boolean value which determine if row is even or odd.
     const even = this.isEven(row);
+
+    // Sum of given row.
     const rowSum = this.rowSum(category, row);
 
+
+    // Check if sum is equal to 1(even) or 5(odd).
     return even ? rowSum === 1 : rowSum === 5;
   }
 
+
+  /**
+   * Get sum of points in given row id and category id.
+   * @param  {number} category - Category id for given mastery id.
+   * @param  {number} row - Row id for given mastery id.
+   * @return {number} Sum of distributed points in given category and row or 0.
+   */
   rowSum(category: number, row: number): number {
+
+    // Take current active masteries.
     return this.current.masteries
+
+      // Filter those which does not match given category and row.
       .filter(mastery => {
         const {category: c, row: r} = this.getInfo(mastery.id);
         return category === c && row === r;
       })
+
+      // Get array of ranks.
       .map(mastery => mastery.rank)
+
+      // Get sum of ranks or 0.
       .reduce((a, b) => a + b, 0);
   }
 
+
+  /**
+   * Update tooltip after updating mastery.
+   * @param  {number} id - Unique mastery id.
+   */
   updateTooltip(id: number): void {
     this.tooltipService.show({
       type: 'mastery',
@@ -118,5 +245,14 @@ export class MasteryService extends Pages<Page> {
         rank: this.current.rank[id]
       }
     });
+  }
+
+  /**
+   * Check if given number is even.
+   * @param  {number} n - Number which will be checked.
+   * @return {boolean} - Boolean value which will determine if `n` is even.
+   */
+  isEven(n: number): boolean {
+    return n % 2 === 0;
   }
 }
